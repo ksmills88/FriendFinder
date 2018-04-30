9d9bbdcb21778509e641ae2fd7fe6b50fa41d3ec
kmills <ksmills82@gmail.com>
2018-04-30 17:23:27 -0400

    updated server.js. started html

---
+++ htmlRoutes.js
@@ -0,0 +1,3 @@
+// Routes: 
+// GET Route to /survey
+// A default catch all route that leads to home.html which displays the home page
--- server.js
+++ server.js
@@ -1,3 +1,20 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
+
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json"}));
+app.use(express.static(__dirname + '/public'));
+
+// req routes
+
+require('./app/routing/htmlRoutes.js')(app);
+require('./app/routing/apiRoutes.js')(app);
+
+app.listen(PORT, function(){
+    console.log("listening on PORT " + PORT);
+})
